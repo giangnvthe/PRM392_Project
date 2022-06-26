38ed62e85f6201c08561bec6d7e8aae86dcd0af9
toanvocuc <86276817+toanvocuc@users.noreply.github.com>
2022-06-26 22:22:40 +0700

    Update User.java
    change missing password db

--- app/src/main/java/com/fptu/android/userinterface/User.java
+++ app/src/main/java/com/fptu/android/userinterface/User.java
@@ -29,4 +29,12 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
